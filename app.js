fa5f47acf6fd66d68dfe0e9e27865e09bcc0053c
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-06-07 22:48:23 -0300

    feat: adicionar rotas getAll e create para usuários

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const z = require("zod");
 const app = express();
 const port = 3030;
+
+app.use(express.json());
 
 // Rota principal para a página inicial
 app.get("/", (req, res) => {
@@ -11,10 +14,23 @@ app.get("/", (req, res) => {
 // Rota GET para CRUD de usuários (ainda sem implementação)
 app.get("/users", (req, res) => {
   // Implemente a lógica para buscar usuários
-  res.send("Lista de Usuários");
+  res.status(200).json({ users: getAllUsers() });
 });
 
 // Implemente outras rotas para criar, atualizar e excluir usuários
+
+// Rota de criação com método POST
+app.post("/users", (req, res) => {
+  const dados = req.body;
+
+  try {
+    validarNovoUsuario(dados);
+    createUser(dados);
+    res.status(201).json({ ...dados });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
@@ -44,3 +60,35 @@ const updateUserById = (userId, newUser) => {
 const deleteUserById = (userId) => {
   users = users.filter((user) => user.id !== userId);
 };
+
+const userSchema = z.object({
+  id: z.number().nonnegative(),
+  name: z.string(),
+  email: z.string().email(),
+  age: z.number().nonnegative(),
+  gender: z.string(),
+});
+
+function validarNovoUsuario(dados) {
+  if (!dados) {
+    throw new Error("Os dados do usuário não podem ser nulos");
+  }
+
+  const eValido = userSchema.safeParse(dados);
+
+  if (!eValido.success) {
+    throw new Error(
+      eValido.error.issues.map((issue) => {
+        const campo = issue.path[0];
+        const message = issue.message;
+        return `${campo}: ${message}\n`;
+      })
+    );
+  }
+
+  const eRepetido = users.find((user) => user.id === dados.id);
+
+  if (eRepetido) {
+    throw new Error(`Id inválido`);
+  }
+}
